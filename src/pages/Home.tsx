55fe8abd49b4482e8a18d9633a5fb74217e954a0
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-12-10 23:42:26 +0900

    [Refactor] Update download button styles and replace SVG icons with images for improved layout and responsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,53 +10,16 @@ export default function Home() {
       <div className={styles.downloadSection}>
         <div className={styles.downloadButtons}>
           <a
-            href="https://play.google.com/store/apps/details?id=com.boombim.android"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.downloadButton}
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M3 20.5V3.5C3 2.91 3.34 2.39 3.84 2.15L13.69 12L3.84 21.85C3.34 21.6 3 21.09 3 20.5Z"
-                fill="#4285F4"
-              />
-              <path
-                d="M16.81 15.12L6.05 21.34L14.54 12.85L16.81 15.12Z"
-                fill="#34A853"
-              />
-              <path
-                d="M20.16 10.81C20.5 11.08 20.75 11.5 20.75 12C20.75 12.5 20.5 12.92 20.16 13.19L17.19 15.53L14.54 12.85L17.19 10.16L20.16 10.81Z"
-                fill="#FBBC04"
-              />
-              <path
-                d="M3.84 2.15L13.69 12L6.05 2.66L3.84 2.15Z"
-                fill="#EA4335"
-              />
-            </svg>
-            Google Play
-          </a>
-          <a
             href="https://apps.apple.com/kr/app/%EB%B6%90%EB%B9%94-boombim/id6751637320"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.downloadButton}
           >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="#000000"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z" />
-            </svg>
-            App Store
+            <img
+              src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/ko-kr.svg"
+              alt="Download on the App Store"
+              style={{ height: "60px" }}
+            />
           </a>
         </div>
       </div>
